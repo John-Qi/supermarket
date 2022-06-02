2f9b3815d97f2c728a88925a28058b70461b2d34
qijq <qijq@amberdata.cn>
2022-06-02 11:57:28 +0800

    配置别名

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack:{
+    resolve:{
+     alias:{
+      'assets': '@/assets',
+      'components': '@/components',
+      'views': '@/views',
+      'types': '@/types',
+      'common': '@/common',
+      'network':"@/network"
+     }
+    }
+  }
 })
